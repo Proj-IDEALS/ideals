3e9a86b9a92ed5491aa1ccfda9a13f2285ffd485
Isuranga Perera <isurangamperera@gmail.com>
2023-02-16 22:04:40 -0600

    [#184418154] Add the initial template

---
+++ api/db/migrate/20230217031739_create_widgets.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class CreateWidgets < ActiveRecord::Migration[7.0]
+  def change
+    create_table :widgets do |t|
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
