52828adbdc7a57558016635fd6759c21d561a47f
Isuranga Perera <isurangamperera@gmail.com>
2023-02-28 18:10:25 -0600

    Add graph container

--- app/config/application.rb
+++ app/config/application.rb
@@ -11,7 +11,6 @@ module App
     config.load_defaults 7.0
     config.log_level = :debug
     config.log_tags  = [:subdomain, :uuid]
-    config.logger    = ActiveSupport::TaggedLogging.new(Logger.new(STDOUT))
     config.assets.paths << Rails.root.join('node_modules')
 
     config.cache_store = :redis_store, ENV['CACHE_URL'],
