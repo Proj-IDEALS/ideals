74b0dea069828964153a1ac65c68a279f9088dd3
Sidharth Anil <sidharthanil98@gmail.com>
2023-03-06 10:31:28 -0600

    cucumber init

---
+++ app/features/step_definitions/test_steps.rb
@@ -0,0 +1,12 @@
+Given('today is Sunday') do
+    # pending # Write code here that turns the phrase above into concrete actions
+    visit root_path
+  end
+  
+  When("I ask whether it's Friday yet") do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then('I should be told {string}') do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
