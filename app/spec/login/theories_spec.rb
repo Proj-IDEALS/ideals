f06f6cae14b8cc84f680eba34611f2c526b9c156
xyTom <75466119+xyTom@users.noreply.github.com>
2023-03-29 05:56:55 +0000

    Add login test cases

---
+++ app/spec/login/theories_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+
+RSpec.describe TheoriesController, type: :controller do
+    describe "updates" do
+        context "when user is not logged in and try to updates an Theorys" do
+          it "redirects to the root path" do
+            theory = Theory.create(name: 'Theory test', description: 'Description for theory test')
+              get :update, params: { id: theory.id, theory: { description: 'New description for theory test' } }
+            expect(response).to redirect_to(root_path)
+            theory.destroy
+          end
+      end
+    end
+
+      describe "destroys" do
+        context "when user is not logged in and try to destroys an Theorys" do
+          it "redirects to the root path" do
+            theory = Theory.create(name: 'Theory test', description: 'Description for theory test')
+              get :destroy, params: {id: theory.id}
+            expect(response).to redirect_to(root_path)
+            theory.destroy
+          end
+      end
+    end
+
+    describe "creates" do
+      context "when user is not logged in and try to create an theory" do
+        it "redirects to the root path" do
+            get :create
+          expect(response).to redirect_to(root_path)
+        end
+    end
+      context "when user is logged in" do
+        before do
+            sign_in_as_test_user
+        end
+  
+        it "allow to create" do
+          get :create, params: {:theory => {:name => "Theory 1", 
+                        :description => "Description for Theory 1"} }
+            
+            theory1 = Theory.find_by(name:"Theory 1")
+            
+            expect(response).to redirect_to theory_path(theory1)
+            expect(flash[:notice]).to match(/Theory was successfully created./)
+        end
+      end
+    end
+
+
+  end
+  
