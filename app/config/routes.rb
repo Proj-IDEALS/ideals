b8d4d54650da9a9d81c89c6f966926a4af7d5d40
xyTom <75466119+xyTom@users.noreply.github.com>
2023-03-29 03:52:30 +0000

    add user info page

--- app/config/routes.rb
+++ app/config/routes.rb
@@ -13,5 +13,7 @@ Rails.application.routes.draw do
   get '/auth/auth0/callback' => 'auth0#callback'
   get '/auth/failure' => 'auth0#failure'
   get '/auth/logout' => 'auth0#logout'
+  get '/user/info' =>  'user#show'
+
 
 end
