5087d7ed603ad6eae6335737fd9fa6767c170469
Sidharth Anil <sidharthanil98@gmail.com>
2023-04-03 19:57:43 -0500

    Added rich text boxes for assumptions and practices too

--- app/app/models/assumption.rb
+++ app/app/models/assumption.rb
@@ -1,5 +1,6 @@
 class Assumption < ApplicationRecord
     has_and_belongs_to_many :theories
     has_and_belongs_to_many :practices
+    has_rich_text :description
     validates :name, presence: true
 end
