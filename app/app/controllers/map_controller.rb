fbbf61af887040f659ebca28efab934733a9fa8a
Isuranga Perera <isurangamperera@gmail.com>
2023-04-07 10:57:38 -0500

    Add graph interactions

--- app/app/controllers/map_controller.rb
+++ app/app/controllers/map_controller.rb
@@ -7,11 +7,17 @@ class MapController < ApplicationController
     assumptions = Assumption.select("id, name, description")
     theories = Theory.select("id, name, description")
     practices = Practice.select("id, name, description")
+    assumptions_practices = Assumption.joins(:assumptions_practices)
+           .select('assumptions_practices.assumption_id, assumptions_practices.practice_id')
+    assumptions_theories = Assumption.joins(:assumptions_theories)
+                                      .select('assumptions_theories.assumption_id, assumptions_theories.theory_id')
 
     nodes = {
       'assumptions' => assumptions,
       'theories' => theories,
-      'practices' => practices
+      'practices' => practices,
+      'assumptions_practices' => assumptions_practices,
+      'assumptions_theories' => assumptions_theories
     }
 
     render json: nodes
--- app/app/frontend/packs/map.js
+++ app/app/frontend/packs/map.js
@@ -25,6 +25,113 @@ function wrapText(text, maxWidth) {
     return lines.join('\n');
 }
 
+function fadeOutUnconnectedAndCenter(graph, nodeId) {
+    // Get connected nodes and edges
+    const connectedNodeIds = new Set([nodeId]);
+    const connectedEdgeIds = new Set();
+
+    graph.getEdges().forEach(edge => {
+        if (edge.getSource().get('id') === nodeId || edge.getTarget().get('id') === nodeId) {
+            connectedNodeIds.add(edge.getSource().get('id'));
+            connectedNodeIds.add(edge.getTarget().get('id'));
+            connectedEdgeIds.add(edge.get('id'));
+        }
+    });
+
+    const connectedNodes = [];
+    const connectedNodePositions = {
+        minX: Infinity,
+        maxX: -Infinity,
+        minY: Infinity,
+        maxY: -Infinity,
+    };
+    // Fade out unconnected nodes, node text, and edges
+    graph.getNodes().forEach(node => {
+        const id = node.get('id');
+        if (!connectedNodeIds.has(id)) {
+            node.update({
+                style: {
+                    opacity: 0,
+                },
+                labelCfg: {
+                    style: {
+                        opacity: 0,
+                    },
+                },
+            });
+        } else {
+            const model = node.getModel();
+            connectedNodes.push(node);
+            connectedNodePositions.minX = Math.min(connectedNodePositions.minX, model.x);
+            connectedNodePositions.maxX = Math.max(connectedNodePositions.maxX, model.x);
+            connectedNodePositions.minY = Math.min(connectedNodePositions.minY, model.y);
+            connectedNodePositions.maxY = Math.max(connectedNodePositions.maxY, model.y);
+        }
+    });
+
+    graph.getEdges().forEach(edge => {
+        if (!connectedEdgeIds.has(edge.get('id'))) {
+            edge.update({
+                style: {
+                    opacity: 0,
+                },
+            });
+        }
+    });
+    // Calculate the center of the connected nodes and the center of the canvas
+    const connectedCenter = {
+        x: (connectedNodePositions.minX + connectedNodePositions.maxX) / 2,
+        y: (connectedNodePositions.minY + connectedNodePositions.maxY) / 2,
+    };
+
+    const canvasCenter = {
+        x: graph.getWidth() / 2,
+        y: graph.getHeight() / 2,
+    };
+
+    // Calculate the translation needed to move the connected nodes to the center
+    const translation = {
+        x: canvasCenter.x - connectedCenter.x,
+        y: canvasCenter.y - connectedCenter.y,
+    };
+
+    // Apply the translation to the connected nodes
+    connectedNodes.forEach(node => {
+        const model = node.getModel();
+        node.updatePosition({
+            x: model.x + translation.x,
+            y: model.y + translation.y,
+        });
+    });
+
+    graph.refreshPositions();
+}
+
+function resetGraph() {
+    graph.getNodes().forEach(node => {
+        node.update({
+            style: {
+                opacity: 1,
+            },
+            labelCfg: {
+                style: {
+                    opacity: 1,
+                },
+            },
+        });
+    });
+
+    graph.getEdges().forEach(edge => {
+        edge.update({
+            style: {
+                opacity: 1,
+            },
+        });
+    });
+
+    graph.refreshPositions();
+}
+
 const width = document.getElementById('concept-map').scrollWidth;
 const height = document.getElementById('concept-map').scrollHeight || 700;
 
@@ -60,7 +167,7 @@ const graph = new G6.Graph({
         linkDistance: 500
     },
     defaultNode: {
-        type: 'modelRect',
+        type: 'rect',
         size: [160, 190],
         style: {
             stroke: '#658FF9',
@@ -73,12 +180,6 @@ const graph = new G6.Graph({
                 fontSize: 15,
             }
         },
-        preRect: {
-            show: true,
-            width: 4,
-            fill: '#40a9ff',
-            radius: 2,
-        },
         linkPoints: {
             top: false,
             right: false,
@@ -88,12 +189,6 @@ const graph = new G6.Graph({
             lineWidth: 1,
             fill: '#72CC4A',
             stroke: '#72CC4A',
-        },
-        logoIcon: {
-            show: false,
-        },
-        stateIcon: {
-            show: false,
         }
     },
     defaultEdge: {
@@ -132,11 +227,29 @@ fetch("api/v1/map")
             });
         });
 
+        data['assumptions_practices'].forEach((edge) => {
+            graphData.edges.push({
+                source: ('assumptions' + edge['assumption_id'].toString()).toString(),
+                target: ('practices' + edge['practice_id'].toString()).toString()
+            })
+        });
+
+        data['assumptions_theories'].forEach((edge) => {
+            graphData.edges.push({
+                source: ('assumptions' + edge['assumption_id'].toString()).toString(),
+                target: ('theories' + edge['theory_id'].toString()).toString()
+            })
+        });
+
         graphData.nodes.forEach(function (node) {
             node.label = wrapText(node.label, 80);
         });
+        console.log(graphData);
         graph.data(graphData);
         graph.render();
+
+        //fadeOutUnconnected('assumptions9');
+        //resetGraph();
     });
 
 
@@ -149,3 +262,48 @@ graph.on('node:mouseleave', (evt) => {
     const {item} = evt;
     graph.setItemState(item, 'hover', false);
 });
+
+function fadeOutUnconnected(nodeId) {
+    resetGraph();
+    // Get connected nodes and edges
+    const connectedNodeIds = new Set([nodeId]);
+    const connectedEdgeIds = new Set();
+
+    graph.getEdges().forEach(edge => {
+        if (edge.getSource().get('id') === nodeId || edge.getTarget().get('id') === nodeId) {
+            connectedNodeIds.add(edge.getSource().get('id'));
+            connectedNodeIds.add(edge.getTarget().get('id'));
+            connectedEdgeIds.add(edge.get('id'));
+        }
+    });
+    // Fade out unconnected nodes and edges
+    graph.getNodes().forEach(node => {
+        if (!connectedNodeIds.has(node.get('id'))) {
+            node.update({
+                style: {
+                    opacity: 0,
+                },
+                labelCfg: {
+                    style: {
+                        opacity: 0,
+                    },
+                }
+            });
+        }
+    });
+
+    graph.getEdges().forEach(edge => {
+        if (!connectedEdgeIds.has(edge.get('id'))) {
+            edge.update({
+                style: {
+                    opacity: 0,
+                },
+            });
+        }
+    });
+
+    graph.refreshPositions();
+}
+
+window.fadeOutUnconnected = fadeOutUnconnected;
+window.resetGraph = resetGraph;
