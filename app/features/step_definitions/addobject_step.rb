091f8c7069253f6bfe77d1e4638486818b180b69
Sidharth Anil <sidharthanil98@gmail.com>
2023-04-14 12:13:37 -0500

    Fixed failing description rich text tests with fill_in_rich_text function and javascript capybara driver, with fake favicon included

--- app/features/step_definitions/addobject_step.rb
+++ app/features/step_definitions/addobject_step.rb
@@ -1,12 +1,16 @@
 # frozen_string_literal: true
 
+def fill_in_rich_text_area(locator = nil, with:)
+  find("trix-editor").execute_script("this.editor.loadHTML(arguments[0])", with.to_s)
+end
+
 Given('I am on the new theory page') do
   visit new_theory_path
 end
 
 When('I fill in the theory form with valid data') do
   fill_in 'Name', with: 'Core Theory 1'
-  fill_in 'Description', with: 'How to learning.'
+  fill_in_rich_text_area 'theory_description', with: 'How to learning.'
 end
 
 When('I click the {string} button') do |button_name|
@@ -15,8 +19,8 @@ end
 
 Then("I should see the new theory's details") do
   theory = Theory.last
-  expect(page).to have_content(theory.name)
-  expect(page).to have_content(theory.description)
+  expect(page).to have_content('Core Theory 1')
+  expect(page).to have_content('How to learning.')
 end
 
 Given('I am on the new assumption page') do
@@ -25,13 +29,13 @@ end
 
 When('I fill in the assumption form with valid data') do
   fill_in 'Name', with: 'Assumption 1'
-  fill_in 'Description', with: 'The assumption mentions that........'
+  fill_in_rich_text_area 'assumption_description', with: 'The assumption mentions that........'
 end
 
 Then("I should see the new assumption's details") do
   assumption = Assumption.last
-  expect(page).to have_content(assumption.name)
-  expect(page).to have_content(assumption.description)
+  expect(page).to have_content('Assumption 1')
+  expect(page).to have_content('The assumption mentions that........')
 end
 
 Given('I am on the new practice page') do
@@ -40,11 +44,11 @@ end
 
 When('I fill in the practice form with valid data') do
   fill_in 'Name', with: 'Practice 1'
-  fill_in 'Description', with: 'Here is the way for how to improve the skill.'
+  fill_in_rich_text_area 'practice_description', with: 'Here is the way for how to improve the skill.'
 end
 
 Then("I should see the new practice's details") do
   practice = Practice.last
-  expect(page).to have_content(practice.name)
-  expect(page).to have_content(practice.description)
+  expect(page).to have_content('Practice 1')
+  expect(page).to have_content('Here is the way for how to improve the skill.')
 end
